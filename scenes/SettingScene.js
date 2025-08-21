2d4a87a494831cbb4ffb35e8c967a74587a22d2b
Function2099 <s36164296@gmail.com>
2025-08-21 13:36:46 +0800

    修正螢幕縮放導致的遊戲比例異常

--- scenes/SettingScene.js
+++ scenes/SettingScene.js
@@ -4,7 +4,11 @@ export class SettingsScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(400, 200, '按鍵設定', { fontSize: '28px', fill: '#fff' })
+    const centerX = this.cameras.main.width / 2;
+    const centerY = this.cameras.main.height / 2;
+    const buttonSpacing = 30;
+
+    this.add.text(centerX, centerY - buttonSpacing, '按鍵設定', { fontSize: '28px', fill: '#fff' })
       .setOrigin(0.5)
       .setInteractive()
       .on('pointerdown', () => {
@@ -12,11 +16,19 @@ export class SettingsScene extends Phaser.Scene {
         // this.scene.start('KeyConfigScene');
       });
 
-    this.add.text(400, 300, '聲音設定', { fontSize: '28px', fill: '#fff' })
+    this.add.text(centerX, centerY + buttonSpacing, '聲音設定', { fontSize: '28px', fill: '#fff' })
       .setOrigin(0.5)
       .setInteractive()
       .on('pointerdown', () => {
         // 這裡可以跳轉到 SoundConfigScene 或開啟音量調整 UI
       });
+    this.add.text(centerX, centerY + 2 * buttonSpacing, '返回主畫面', { fontSize: '28px', fill: '#00f0ff' })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true })
+    backText.on('pointerover', () => backText.setStyle({ fill: '#00c0cc' }));
+    backText.on('pointerout', () => backText.setStyle({ fill: '#00f0ff' }));
+    backText.on('pointerdown', () => {
+      this.scene.start('MainMenuScene'); // 替換成你的主畫面 scene 名稱
+    });
   }
 }
